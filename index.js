6c2c10d20cf18f72568b2940c21c0256faa6b36b
wmik <williemik.wmik@gmail.com>
2019-05-18 09:16:43 +0300

    fix: next argument assignment
    - Uses a `function` type check to identify `next` argument
    - Test
    
    fix #28

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const asyncUtil = fn =>
-function asyncUtilWrap(...args) {
-  const fnReturn = fn(...args)
-  const next = args[args.length-1]
-  return Promise.resolve(fnReturn).catch(next)
-}
+  function asyncUtilWrap(...args) {
+    const fnReturn = fn(...args);
+    const next = args.find(arg => typeof arg === 'function');
+    return Promise.resolve(fnReturn).catch(next);
+  };
 
-module.exports = asyncUtil
+module.exports = asyncUtil;
--- test.js
+++ test.js
@@ -1,100 +1,110 @@
-const chai = require('chai')
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
-const { expect } = chai
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const { expect } = chai;
 
-chai.use(sinonChai)
-chai.should()
+chai.use(sinonChai);
+chai.should();
 
-
-
-const asyncUtil = require('./index')
+const asyncUtil = require('./index');
 
 describe('asyncUtil', () => {
-
   it('should catch exceptions of a function passed into it', async () => {
-    const error = new Error('catch me!')
+    const error = new Error('catch me!');
     const foo = asyncUtil(() => {
-      throw error
-    })
-    expect(foo).to.throw(error)
-  })
+      throw error;
+    });
+    expect(foo).to.throw(error);
+  });
 
   it('should call next with the error when an async function passed into it throws', async () => {
-    const error = new Error('catch me!')
+    const error = new Error('catch me!');
     const next = sinon.spy();
     const foo = asyncUtil(async (req, res, next) => {
-      throw error
-    })
+      throw error;
+    });
 
-    await foo(null, null, next)
-    expect(next).to.have.been.calledWith(error)
-  })
+    await foo(null, null, next);
+    expect(next).to.have.been.calledWith(error);
+  });
 
   it('should call next with the arguments when an async function passed into it calls next', async () => {
-    const next = sinon.spy()
+    const next = sinon.spy();
     const foo = asyncUtil(async (req, res, next) => {
-      next('test')
-    })
+      next('test');
+    });
 
-    await foo(null, null, next)
-    expect(next).to.have.been.calledWith('test')
-  })
+    await foo(null, null, next);
+    expect(next).to.have.been.calledWith('test');
+  });
 
   it('should provide additional arguments to the middleware', async () => {
-    const next = sinon.spy()
+    const next = sinon.spy();
     const id = '1';
     const foo = asyncUtil(async (req, res, next, id) => {
       return id;
-    })
+    });
 
     const result = await foo(null, null, next, id);
     expect(result).to.equal(id);
-  })
+  });
+
+  it('should assign the appropriate argument to `next`', async () => {
+    const next = sinon.spy();
+    const id = '1';
+    const error = new Error('some err!!!');
+    const foo = asyncUtil(async (req, res, next, id) => {
+      throw error;
+    });
+
+    const result = await foo(null, null, next, id);
+    expect(result).to.be.undefined;
+    expect(next).to.have.been.calledWith(error);
+  });
 
   it('should accept a non-async function', async () => {
-    const next = sinon.spy()
-    const foo = asyncUtil((req, res, next) => {
-      next('test')
-    })
-
-    await foo(null, null, next)
-    expect(next).to.have.been.calledWith('test')
-  })
-
-  it('should accept a non-async function erroring', async () => {
-    const error = new Error('catch me!')
     const next = sinon.spy();
     const foo = asyncUtil((req, res, next) => {
-      next(error)
-    })
+      next('test');
+    });
 
-    await foo(null, null, next)
-    expect(next).to.have.been.calledWith(error)
-  })
+    await foo(null, null, next);
+    expect(next).to.have.been.calledWith('test');
+  });
+
+  it('should accept a non-async function erroring', async () => {
+    const error = new Error('catch me!');
+    const next = sinon.spy();
+    const foo = asyncUtil((req, res, next) => {
+      next(error);
+    });
+
+    await foo(null, null, next);
+    expect(next).to.have.been.calledWith(error);
+  });
 
   // NB, thenables are not guaranteed to have a `catch` method.
   it('should handle thenables', async () => {
-    const error = Error('catch me!')
+    const error = Error('catch me!');
     // construct a minimalist thenable which we can fail at a specific time
-    let thenable, triggerFailure
+    let thenable, triggerFailure;
     const registeringThenable = new Promise(res => {
       thenable = {
         then: sinon.spy((success, fail) => {
-          triggerFailure = fail
-          res()
+          triggerFailure = fail;
+          res();
         })
-      }
-    })
+      };
+    });
 
     // test the actual library feature
-    const next = sinon.spy()
-    const catchingThenable = asyncUtil(_ => thenable)(null, null, next)
-    await registeringThenable
-    expect(thenable.then).to.have.been.called
-    expect(next).not.to.have.been.called
-    triggerFailure(error)
-    await catchingThenable
-    expect(next).to.have.been.calledWith(error)
-  })
-})
+    const next = sinon.spy();
+    const catchingThenable = asyncUtil(_ => thenable)(null, null, next);
+    await registeringThenable;
+    expect(thenable.then).to.have.been.called;
+    expect(next).not.to.have.been.called;
+    triggerFailure(error);
+    await catchingThenable;
+    expect(next).to.have.been.calledWith(error);
+  });
+});
